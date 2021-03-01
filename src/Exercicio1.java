e406744543cf811885cdc1e9f2adcb57653044bd
Vinicius do Carmo Costa <vincosta@cisatrading.com.br>
2021-03-01 20:46:17 -0300

    nivelamento java

---
+++ src/Exercicio1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, soma;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		soma = A + B;
+		System.out.println("O valor da soma é = " + soma);
+		
+		sc.close();
+	}
+
+}
